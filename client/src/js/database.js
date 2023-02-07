1c1f6cccf71a1a9d0755a278be6f459e5e51270e
Nevin Lewis <lewisnevin@utexas.edu>
2023-02-06 21:39:06 -0600

    changed path

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,11 +15,8 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log("Posting to DB");
-
   const jateDb = await openDB("jate", 1);
-
   const tx = jateDb.transaction("jate", "readwrite");
-
   const store = tx.objectStore("jate");
 
   const request = store.put({ id: 1, content: content });
